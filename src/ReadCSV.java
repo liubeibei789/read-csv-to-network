6fa477f5d5496f0bf98ee18e840de57e2279d15b
Beibei LIU <liubeibei789@gmail.com>
2018-04-11 14:44:15 -0700

    Add files via upload

--- src/ReadCSV.java
+++ src/ReadCSV.java
@@ -1,5 +1,11 @@
+// this class reads the directories of CSV files,
+// which contains node information and link information,
+// to create a network
 
-
+// node type: 1:sdn switch; 2:legacy switch
+// link type: 1:sdn-sdn link; 2:legacy-legacy link; 3:sdn-legacy link
+// linkFrom:start node; linkTo:end node
+// -1:start host,-2:end host
 
 package src;
 
@@ -18,30 +24,33 @@ public class ReadCSV {
     private int[] linkWeight = new int[30];
     private int[] linkType = new int[30];
 
-//    public ReadCSV(String filePath1, String filePath2) {
-//        System.out.println("reading csv files...");
-//        read(filePath1, filePath2);
-//    }
+    public ReadCSV(String filePath1, String filePath2) {
+        System.out.println("reading csv files...");
+        read(filePath1, filePath2);
+    }
 
     public ReadCSV() {
         System.out.println("reading default csv files...");
-        String filePath1 = "/Users/beibei/Desktop/toy_node.csv";
-        String filePath2 = "/Users/beibei/Desktop/toy_edge.csv";
+        String filePath1 = "./toy_node.csv";
+        String filePath2 = "./toy_edge.csv";
         read(filePath1, filePath2);
     }
 
     public int[] read(String filePath1, String filePath2){
 
-        int i = 0, j = 0;
-        int[] num = new int[2];
+        int i = 0, j = 0;  //i:index for nodes;j:index for links
+        int[] num = new int[2];//total number of nodes and links
+
+        //================= take node info ===================
         try {
             CsvReader csvReader = new CsvReader(filePath1);
             csvReader.readHeaders();
-            System.out.println("node raw data:");
+            //uncomment the following 3 lines with *, if you want to see raw csv records
+            //System.out.println("node raw data:");   //*
 
             while (csvReader.readRecord()){
-                System.out.printf("i=%d,",i);
-                System.out.println(csvReader.getRawRecord());
+                //System.out.printf("i=%d,",i);  //*
+                //System.out.println(csvReader.getRawRecord());  //*
                 nodeId[i] = Integer.parseInt(csvReader.get("id"));
                 nodeType[i] = Integer.parseInt(csvReader.get("type"));
                 nodeWeight[i] = Integer.parseInt(csvReader.get("weight"));
@@ -51,14 +60,15 @@ public class ReadCSV {
             e.printStackTrace();
         }
 
-
+        //================= take link info ===================
         try {
             CsvReader csvReader2 = new CsvReader(filePath2);
             csvReader2.readHeaders();
-            System.out.println("link raw data:");
+            //uncomment the following 2 lines with *, if you want to see raw csv records
+            //System.out.println("link raw data:");    //*
 
             while (csvReader2.readRecord()){
-                System.out.println(csvReader2.getRawRecord());
+                //System.out.println(csvReader2.getRawRecord());   //*
                 linkFrom[j] = Integer.parseInt(csvReader2.get("from"));
                 linkTo[j] = Integer.parseInt(csvReader2.get("to"));
                 linkWeight[j] = Integer.parseInt(csvReader2.get("weight"));
@@ -75,7 +85,7 @@ public class ReadCSV {
         return num;
     }
 
-    // nodes:
+    //------------- output node info -------------
     public int [] getNodeId() {
         return nodeId;
     }
@@ -86,8 +96,7 @@ public class ReadCSV {
         return nodeWeight;
     }
 
-    // links:
-
+    //------------- output link info -------------
     public int [] getLinkFrom() {
         return linkFrom;
     }
